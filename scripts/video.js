00cd25f14a8029df4e54a6ee1886889fbade1723
Biswa Mitra Barua <biswamitra960@gmail.com>
2024-10-04 00:53:38 +0600

    search option added dynamically and video details also and completed

--- scripts/video.js
+++ scripts/video.js
@@ -24,9 +24,9 @@ const loadCategories = () => {
         .catch(error => console.log(error))
 }
 
-const loadVideos = () => {
+const loadVideos = (searchText = "") => {
     //fetch the videos
-    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
         .then(res => res.json())
         .then(data => displayVideos(data.videos))
         .catch(error => console.log(error))
@@ -164,6 +164,10 @@ const displayCategories = (categories) => {
         categoryContainer.append(buttonContainer);
     })
 }
+
+document.getElementById('search-input').addEventListener('keyup', (e) =>{
+    loadVideos(e.target.value)
+} )
 
 loadCategories();
 loadVideos();
